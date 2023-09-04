ad2f13aba31c9d9b0be45c6a75b7708f7b41d0de
Rabi03 <islamrabi02@gmail.com>
2023-09-04 17:56:42 +0600

    update

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -10,8 +10,11 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 const Modal = ({ modal, setModal, type }) => {
   const [title, setTitle] = useState("")
   const [video, setVideo] = useState("")
+  const [text,setText]=useState('')
   const [file, setFile] = useState('')
+  const [image, setImage] = useState('')
   const [percent, setPercent] = useState(0)
+  const [noticetype, setNoticeType] = useState('txt')
   //For Image Preview
   const [selectedImage, setSelectedImage] = useState();
 
@@ -45,6 +48,36 @@ const Modal = ({ modal, setModal, type }) => {
 
   };
 
+  const handleImageUpload = (event) => {
+    let file = event.target.files[0];
+    const storageRef = ref(storage, `/files/${file.name}`);
+
+    // progress can be paused and resumed. It also exposes progress updates.
+    // Receives the storage reference and the file to upload.
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+
+        // update progress
+        setPercent(percent);
+      },
+      (err) => alert(err),
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setImage(url);
+        });
+      }
+    );
+
+  };
+
+
   // This function will be triggered when the "Remove This Image" button is clicked
   const removeSelectedImage = () => {
     setSelectedImage();
@@ -57,7 +90,13 @@ const Modal = ({ modal, setModal, type }) => {
       await addData("video", v4(), { title, video, date: Date.now() })
     }
     else {
+      if(noticetype==='file')
       await addData("file", v4(), { title, file, date: Date.now() })
+    else if(noticetype==='txt')
+    await addData("text", v4(), { title, text, date: Date.now() })
+    else
+    await addData("image", v4(), { title, image, date: Date.now() })
+
     }
     setModal(false)
     window.location.reload()
@@ -93,32 +132,50 @@ const Modal = ({ modal, setModal, type }) => {
             <label className="font-semibold pr-2">Video Link</label>
             <input className="border-2 border-purple-600/50 w-[75%] " type="text" onChange={e => setVideo(e.target.value)} />
           </div> :
-            <div className="flex-row justify-between">
-              <label className="font-semibold pr-2">Upload file (PDF,TXT)</label>
-              <input
-                className="border-2"
-                type="file"
-                accept="application/*"
-                name="user[image]"
-                multiple={true}
-                onChange={handleUpload}
-              />
-              <div className="flex overflow-auto my-2 p-2">
-                {
-                  selectedImage && [...selectedImage].map((file, index) => <img key={index} src={URL.createObjectURL(file)} className="w-32 h-32 mr-1 rounded-sm border-4" />)
-                }
-
+            <>
+              <div className="flex items-center gap-1">
+                <input type="radio" id="html" name="fav_language" value="txt" onChange={()=>setNoticeType('txt')} checked={noticetype==='txt'} />
+                <label for="html">Text</label><br />
+                <input type="radio" id="css" name="fav_language" value="file" onChange={()=>setNoticeType('file')} checked={noticetype==='file'} />
+                <label for="css">File</label><br />
+                <input type="radio" id="image" name="fav_language" value="image" onChange={()=>setNoticeType('image')} checked={noticetype==='image'} />
+                <label for="image">Image</label><br />
               </div>
+              {(noticetype === 'file'||noticetype==='image') ?
+                <div className="flex-row justify-between">
+                  <label className="font-semibold pr-2">{noticetype==='file'?'Upload file (PDF,TXT)':"Upload Image"}</label>
+                  <input
+                    className="border-2"
+                    type="file"
+                    accept={noticetype==='file'?"application/*":"image/*"}
+                    name="user[image]"
+                    multiple={true}
+                    onChange={noticetype==='file'? handleUpload:handleImageUpload}
+                  />
+                  <div className="flex overflow-auto my-2 p-2">
+                    {
+                      selectedImage && [...selectedImage].map((file, index) => <img key={index} src={URL.createObjectURL(file)} className="w-32 h-32 mr-1 rounded-sm border-4" />)
+                    }
 
-              {selectedImage &&
-                <button onClick={removeSelectedImage} className='bg-orange-400 p-2 rounded-md text-white'>
-                  Remove This Image
-                </button>
+                  </div>
+
+                  {selectedImage &&
+                    <button onClick={removeSelectedImage} className='bg-orange-400 p-2 rounded-md text-white'>
+                      Remove This Image
+                    </button>
+                  }
+
+                  {percent > 0 && <p>Please Wait... {percent}%</p>}
+
+                </div>
+                :
+
+                <div className="flex justify-between">
+                  <label className="font-semibold pr-2">Your Text</label>
+                  <textarea rows={10} className="border-2 border-purple-600/50 w-[75%] " type="text" onChange={e => setText(e.target.value)} />
+                </div>
               }
-
-              {percent>0&&<p>Please Wait... {percent}%</p>}
-
-            </div>
+            </>
           }
 
           <div className="flex justify-between">
--- src/components/sub-category/SubCategoryTable.js
+++ src/components/sub-category/SubCategoryTable.js
@@ -1,8 +1,8 @@
 import Table from 'rc-table';
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pagination from "react-js-pagination";
 import { HiDocumentText } from 'react-icons/hi'
-import {BsFillFileEarmarkPdfFill} from 'react-icons/bs'
+import { BsFillFileEarmarkPdfFill,BsImageFill } from 'react-icons/bs'
 import getAllDouments from '@/firebase/getData';
 import Link from 'next/link';
 
@@ -10,6 +10,8 @@ const SubCategoryTable = () => {
 
 
   const [fileData, setfileData] = useState([])
+  const [textData,setTextData]=useState([])
+  const [imageData,setImageData]=useState([])
 
   const getAllfile = async () => {
     const { result, error } = await getAllDouments("file")
@@ -29,9 +31,49 @@ const SubCategoryTable = () => {
     }
   }
 
+  const getAllText = async () => {
+    const { result, error } = await getAllDouments("text")
+    let data = []
+    if (result) {
+
+      result.forEach((doc) => {
+        let { title, date, text } = doc.data()
+        data.push({ title, text, date: new Date(date).toLocaleDateString() })
+      });
+
+      setTextData(data)
+
+    }
+    else {
+      alert(error)
+    }
+  }
+
+  const getAllImage = async () => {
+    const { result, error } = await getAllDouments("image")
+    let data = []
+    if (result) {
+
+      result.forEach((doc) => {
+        let { title, date, image } = doc.data()
+        data.push({ title, image, date: new Date(date).toLocaleDateString() })
+      });
+
+      setImageData(data)
+
+    }
+    else {
+      alert(error)
+    }
+  }
+
   useEffect(() => {
     getAllfile()
+    getAllText()
+    getAllImage()
   }, [])
+
+  console.log(textData)
   return (
     <>
       <table class="table-auto w-full">
@@ -46,6 +88,7 @@ const SubCategoryTable = () => {
             <th class="p-2 whitespace-nowrap">
               <div class="font-semibold text-left">File</div>
             </th>
+            
 
           </tr>
         </thead>
@@ -60,9 +103,84 @@ const SubCategoryTable = () => {
               <td class="p-2 whitespace-nowrap">
                 <div class="text-left">{data.date}</div>
               </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="text-left font-medium text-green-500"><Link href={data.file}><BsFillFileEarmarkPdfFill size={25} /></Link></div>
+                </td>
+              
+
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+      <table class="table-auto w-full">
+        <thead class="text-xl text-center font-semibold uppercase text-gray-400 bg-gray-50">
+          <tr>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Title</div>
+            </th>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Date</div>
+            </th>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Text</div>
+            </th>
+            
+
+          </tr>
+        </thead>
+        <tbody class="text-xl divide-y divide-gray-100">
+          {textData.map((data, index) => (
+            <tr>
               <td class="p-2 whitespace-nowrap">
-                <div class="text-left font-medium text-green-500"><Link href={data.file}><BsFillFileEarmarkPdfFill size={25} /></Link></div>
+                <div class="flex items-center">
+                  <div class="font-medium text-gray-800">{data.title}</div>
+                </div>
               </td>
+              <td class="p-2 whitespace-nowrap">
+                <div class="text-left">{data.date}</div>
+              </td>
+                <td class="p-2 w-[50%]">
+                  <div class="text-left font-medium text-green-500">{data.text}</div>
+                </td>
+              
+
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+      <table class="table-auto w-full">
+        <thead class="text-xl text-center font-semibold uppercase text-gray-400 bg-gray-50">
+          <tr>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Title</div>
+            </th>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Date</div>
+            </th>
+            <th class="p-2 whitespace-nowrap">
+              <div class="font-semibold text-left">Image</div>
+            </th>
+            
+
+          </tr>
+        </thead>
+        <tbody class="text-xl divide-y divide-gray-100">
+          {imageData.map((data, index) => (
+            <tr>
+              <td class="p-2 whitespace-nowrap">
+                <div class="flex items-center">
+                  <div class="font-medium text-gray-800">{data.title}</div>
+                </div>
+              </td>
+              <td class="p-2 whitespace-nowrap">
+                <div class="text-left">{data.date}</div>
+              </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="text-left font-medium text-green-500"><Link href={data.image}><BsImageFill size={25} /></Link></div>
+                </td>
+              
 
             </tr>
           ))}
